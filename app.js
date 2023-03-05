012263d2bc421a0f6f2fdd57913f39ece0c9917d
himanshuuwu <himanshudubey.studies@gmail.com>
2023-03-05 19:49:11 +0530

    fixes

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ const score = document.querySelector('#score');
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 const newgame = document.querySelector('#new');
-const winscore = document.querySelector('#winscore');
+const winscoreinput = document.querySelector('#winscoreinput');
 const winscorepara = document.querySelector('#winscorepara');
 const winner=document.querySelector('#winner');
+const submitwinscore=document.querySelector('#submitwinscore');
+const winscoretext=document.querySelector('#winscoretext');
 //VARIABLE DECLARATION
 let player1score = 0;
 let player2score = 0;
+let winingscore = 0;
 function displaytoflex() {
     score.style.display = 'flex';
     player1.style.display = 'flex';
     player2.style.display = 'flex';
-    winscorepara.style.display = 'flex';
 }
 //FUNCTION DECLARATIOM
 function result() {
-    let winingscore = winscore.value;
     if (winingscore == player1score || winingscore == player2score) {
         winner.style.display='flex';
         if (winingscore == player1score) {
@@ -36,13 +37,22 @@ function result() {
 }
 //ADDINDING EVENTS
 newgame.addEventListener('click', () => {
-    displaytoflex();
+    winscorepara.style.display = 'flex';
+    //winscoretext.innerText='Set the winning score here...'; 
+    winscoreinput.style.display='flex';
+    submitwinscore.style.display='flex';
     newgame.innerText='NEW';
     player1score = 0;
     player2score = 0;
-    winscore.value=1;
     winner.style.display='none';
     score.innerHTML = `SCORE = ${player1score} : ${player2score}`;
+})
+submitwinscore.addEventListener('click',()=>{
+    displaytoflex();
+    winingscore = winscoreinput.value;
+    winscoreinput.style.display='none';
+    submitwinscore.style.display='none';
+    winscoretext.innerText=`The winning score is ${winingscore}`;
 })
 player1.addEventListener('click', () => {
     player1score++;
